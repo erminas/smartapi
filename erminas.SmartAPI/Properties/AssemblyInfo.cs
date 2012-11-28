a9d467f77d4cfdfc3467c9d89b45bb49e02c7f49
Jonas Jacobi <jonas.jacobi@erminas.de>
2012-11-28 09:10:00 +0100

    Changed version to 0.9.0.0

--- erminas.SmartAPI/Properties/AssemblyInfo.cs
+++ erminas.SmartAPI/Properties/AssemblyInfo.cs
@@ -21,7 +21,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 
-[assembly: AssemblyTitle("erminas Red.NET Library")]
+[assembly: AssemblyTitle("erminas SmartAPI Library")]
 [assembly: AssemblyDescription("Library for programatical access to RedDot Servers")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("erminas")]
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
