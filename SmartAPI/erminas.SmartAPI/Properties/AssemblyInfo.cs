c0e8f41afae5644445d4cb24fd444da1d14951e9
Jonas Jacobi <jonas.jacobi@erminas.de>
2013-04-16 16:43:01 +0200

    change to version 0.9.9.0 (1.0 RC1)

--- SmartAPI/erminas.SmartAPI/Properties/AssemblyInfo.cs
+++ SmartAPI/erminas.SmartAPI/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.4.0")]
-[assembly: AssemblyFileVersion("0.9.4.0")]
+[assembly: AssemblyVersion("0.9.9.0")]
+[assembly: AssemblyFileVersion("0.9.9.0")]
 [assembly: InternalsVisibleTo("erminas Smart API Test")]
