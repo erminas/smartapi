e076e5e14db772ba7acf0e64849f463fdea778ee
Jonas Jacobi <jonas.jacobi@erminas.de>
2013-06-17 10:15:48 +0200

    object model for optionlist entries

--- SmartAPI/erminas.SmartAPI/CMS/Project/ContentClasses/Elements/IOptionList.cs
+++ SmartAPI/erminas.SmartAPI/CMS/Project/ContentClasses/Elements/IOptionList.cs
@@ -13,9 +13,14 @@
 // You should have received a copy of the GNU General Public License along with this program.
 // If not, see <http://www.gnu.org/licenses/>.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 using System.Xml;
 using erminas.SmartAPI.CMS.Project.ContentClasses.Elements.Attributes;
 using erminas.SmartAPI.Utils;
+using erminas.SmartAPI.Utils.CachedCollections;
 
 namespace erminas.SmartAPI.CMS.Project.ContentClasses.Elements
 {
@@ -37,11 +42,68 @@ namespace erminas.SmartAPI.CMS.Project.ContentClasses.Elements
         /// </summary>
         string EntriesAsString { get; set; }
 
+        IEnumerable<IOptionListSelection> Entries { get; }
+
         bool HasLanguageDependendNames { get; set; }
         bool HasLanguageDependendValues { get; set; }
         bool IsAllowingOtherValues { get; set; }
         string SampleText { get; set; }
         SortMode SortMode { get; set; }
+    }
+
+    public interface IOptionListEntry
+    {
+        ILanguageVariant LanguageVariant { get; }
+        string Name { get; }
+        string Value { get; }
+    }
+
+    internal class OptionListEntry : IOptionListEntry
+    {
+        public ILanguageVariant LanguageVariant { get; internal set; }
+        public string Name { get; internal set; }
+        public string Value { get; internal set; }
+    }
+
+    public interface IOptionListSelection : IProjectObject
+    {
+        IOptionList OptionList { get; }
+        Guid Guid { get; }
+        IEnumerable<IOptionListEntry> Entries { get; }
+    }
+
+    internal sealed class OptionListSelection : IOptionListSelection
+    {
+        private readonly IOptionList _optionList;
+
+        public OptionListSelection(IOptionList optionList, XmlElement element)
+        {
+            _optionList = optionList;
+            Guid = element.GetGuid();
+            Entries = (from XmlElement curEntry in element.GetElementsByTagName("ITEM")
+                       select
+                           new OptionListEntry()
+                               {
+                                   LanguageVariant = Project.LanguageVariants[curEntry.GetAttributeValue("languageid")],
+                                   Name = curEntry.GetName(),
+                                   Value = curEntry.InnerText
+                               }).ToList().AsReadOnly();
+        }
+
+        public IOptionList OptionList
+        {
+            get { return _optionList; }
+        }
+
+        public Guid Guid { get; private set; }
+        public IEnumerable<IOptionListEntry> Entries { get; private set; }
+
+        public IProject Project
+        {
+            get { return OptionList.Project; }
+        }
+
+        public ISession Session { get { return OptionList.Session; } }
     }
 
     internal class OptionList : ContentClassContentElement, IOptionList
@@ -75,6 +137,17 @@ namespace erminas.SmartAPI.CMS.Project.ContentClasses.Elements
         {
             get { return GetAttributeValue<string>("eltrddescription"); }
             set { SetAttributeValue("eltrddescription", value); }
+        }
+
+        public IEnumerable<IOptionListSelection> Entries
+        {
+            get
+            {
+                var doc = new XmlDocument();
+                doc.LoadXml(HttpUtility.HtmlDecode(EntriesAsString));
+                return (from XmlElement curEntry in doc.GetElementsByTagName("SELECTION")
+                        select (IOptionListSelection) new OptionListSelection(this, curEntry)).ToList().AsReadOnly();
+            }
         }
 
         public string EntriesAsString
